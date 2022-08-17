0933a6faf87c75d856d60fd3b8acc38a12981960
djsftree <10553134+djsftree@users.noreply.github.com>
2022-08-17 20:42:05 +0100

    Update 10-qt5-check-opengl2.sh

--- files/userscripts/10-qt5-check-opengl2.sh
+++ files/userscripts/10-qt5-check-opengl2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [ -z "$QT_XCB_FORCE_SOFTWARE_OPENGL" ]; then
 QT5_CHECK_OPENGL_VERSION=`LANG=C glxinfo 2> /dev/null | grep '^OpenGL version string: ' | head -n 1 | sed -e 's/^OpenGL version string: \([0-9]\).*$/\1/g'` ||:
-if [ "$QT5_CHECK_OPENGL_VERSION" == "1" ]; then
+if [ "$QT5_CHECK_OPENGL_VERSION" == "2" ]; then
   QT_XCB_FORCE_SOFTWARE_OPENGL=1
   export QT_XCB_FORCE_SOFTWARE_OPENGL
 fi
